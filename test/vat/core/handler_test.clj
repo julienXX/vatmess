ebcd4706b6337f107fecc19b551c3c1fa12c3a74
Julien Blanchard <julien@sideburns.eu>
2014-12-19 17:36:58 +0100

    Add some tests
    Need improvements like JSON parsing the response...

--- test/vat/core/handler_test.clj
+++ test/vat/core/handler_test.clj
@@ -4,10 +4,25 @@
             [vat.core.handler :refer :all]))
 
 (deftest test-app
-  (testing "main route"
+  (testing "index"
     (let [response (app (mock/request :get "/"))]
       (is (= (:status response) 200))
-      (is (= (:body response) "Hello World"))))
+      (is (= (:body response) "{\"desc\":\"This API gives you the VAT rate to use for a given EU ISO country code.\",\"usage\":\"curl -X GET http://vatmess.herokuapp.com/vat_rates/fr\"}"))))
+
+  (testing "vat_rates"
+    (let [response (app (mock/request :get "/vat_rates"))]
+      (is (= (:status response) 200))
+      (is (= (:body response) "{\"vat_rates\":[{\"name\":\"Belgium\",\"rate\":\"21\",\"code\":\"BE\"},{\"name\":\"Bulgaria\",\"rate\":\"20\",\"code\":\"BG\"},{\"name\":\"Czech Republic\",\"rate\":\"21\",\"code\":\"CZ\"},{\"name\":\"Denmark\",\"rate\":\"25\",\"code\":\"DK\"},{\"name\":\"Germany\",\"rate\":\"19\",\"code\":\"DE\"},{\"name\":\"Estonia\",\"rate\":\"20\",\"code\":\"EE\"},{\"name\":\"Greece\",\"rate\":\"23\",\"code\":\"EL\"},{\"name\":\"Spain\",\"rate\":\"21\",\"code\":\"ES\"},{\"name\":\"France\",\"rate\":\"20\",\"code\":\"FR\"},{\"name\":\"Croatia\",\"rate\":\"25\",\"code\":\"HR\"},{\"name\":\"Ireland\",\"rate\":\"23\",\"code\":\"IE\"},{\"name\":\"Italy\",\"rate\":\"22\",\"code\":\"IT\"},{\"name\":\"Cyprus\",\"rate\":\"19\",\"code\":\"CY\"},{\"name\":\"Latvia\",\"rate\":\"21\",\"code\":\"LV\"},{\"name\":\"Lithuania\",\"rate\":\"21\",\"code\":\"LT\"},{\"name\":\"Luxembourg\",\"rate\":\"15\",\"code\":\"LU\"},{\"name\":\"Hungary\",\"rate\":\"27\",\"code\":\"HU\"},{\"name\":\"Malta\",\"rate\":\"18\",\"code\":\"MT\"},{\"name\":\"Netherlands\",\"rate\":\"21\",\"code\":\"NL\"},{\"name\":\"Austria\",\"rate\":\"20\",\"code\":\"AT\"},{\"name\":\"Poland\",\"rate\":\"23\",\"code\":\"PL\"},{\"name\":\"Portugal\",\"rate\":\"23\",\"code\":\"PT\"},{\"name\":\"Romania\",\"rate\":\"24\",\"code\":\"RO\"},{\"name\":\"Slovenia\",\"rate\":\"22\",\"code\":\"SI\"},{\"name\":\"Slovakia\",\"rate\":\"20\",\"code\":\"SK\"},{\"name\":\"Finland\",\"rate\":\"24\",\"code\":\"FI\"},{\"name\":\"Sweden\",\"rate\":\"25\",\"code\":\"SE\"},{\"name\":\"United Kingdom\",\"rate\":\"20\",\"code\":\"UK\"}]}"))))
+
+  (testing "EU vat_rate"
+    (let [response (app (mock/request :get "/vat_rates/fr"))]
+      (is (= (:status response) 200))
+      (is (= (:body response) "{\"code\":\"FR\",\"country\":\"France\",\"vat_rate\":\"20\"}"))))
+
+  (testing "non EU vat_rate"
+    (let [response (app (mock/request :get "/vat_rates/us"))]
+      (is (= (:status response) 200))
+      (is (= (:body response) "{\"code\":\"US\",\"country\":\"Invalid EU country code.\",\"vat_rate\":\"No EU VAT to charge.\"}"))))
 
   (testing "not-found route"
     (let [response (app (mock/request :get "/invalid"))]
